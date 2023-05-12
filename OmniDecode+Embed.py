30c2b04f35c8899353bf945e6d75a1e2e9c0386c
LawyerCat42069 <133282516+LawyerCat42069@users.noreply.github.com>
2023-05-12 11:46:31 -0700

    Update OmniDecode+Embed.py

--- OmniDecode+Embed.py
+++ OmniDecode+Embed.py
@@ -9,18 +9,26 @@ import packet
 
 salt = "Salt6969420741!"
 
-# Hash values
-hash_value1 = "paste_hash_value_1_here"
-hash_value2 = "paste_hash_value_2_here"
+def read_hash_values(file_path):
+    hash_values = {}
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            if line.startswith('Hash Value'):
+                parts = line.split(':')
+                key = parts[0].strip()
+                value = parts[1].strip()
+                hash_values[key] = value
+    return hash_values
 
-# Reverse the hashing process to retrieve the base64 strings
-BRC_69 = base64.b64decode(hash_value1[len(salt):]).decode('utf-8')
-BRC_69_MD = base64.b64decode(hash_value2[len(salt):]).decode('utf-8')
+
+def reverse_hashing(hash_value, salt):
+    return base64.b64decode(hash_value[len(salt):]).decode('utf-8')
 
 
-def decode_base64_to_image(BRC_69, output_path):
+def decode_base64_to_image(base64_string, output_path):
     # Decode base64 string
-    decoded_data = base64.b64decode(BRC_69)
+    decoded_data = base64.b64decode(base64_string)
 
     # Convert decoded data to an image object
     img_data = BytesIO(decoded_data)
@@ -33,9 +41,9 @@ def decode_base64_to_image(BRC_69, output_path):
     return image_path
 
 
-def decode_and_embed_metadata(image_path, BRC_69_MD, output_image_path, output_json_path):
+def decode_and_embed_metadata(image_path, metadata_base64, output_image_path, output_json_path):
     # Decode the hidden metadata from base64
-    decoded_metadata = base64.b64decode(BRC_69_MD)
+    decoded_metadata = base64.b64decode(metadata_base64)
 
     # Convert the decoded metadata from BSON to JSON
     try:
@@ -54,10 +62,18 @@ def decode_and_embed_metadata(image_path, BRC_69_MD, output_image_path, output_j
 
 
 if __name__ == "__main__":
-    image_path = decode_base64_to_image(packet.BRC_69, 'C:\\path\\to\\your\\directory\\decoded_image.bmp')
+    # Read the hash values from the designated text file
+    hash_file_path = 'path/to/your/hash_file.txt'
+    hash_values = read_hash_values(hash_file_path)
+
+    # Reverse the hashing process to retrieve the base64 strings
+    BRC_69 = reverse_hashing(hash_values['Hash Value 1'], salt)
+    BRC_69_MD = reverse_hashing(hash_values['Hash Value 2'], salt)
+
+    image_path = decode_base64_to_image(BRC_69, 'path/to/your/directory/decoded_image.bmp')
     decode_and_embed_metadata(
         image_path,
-        packet.BRC_69_MD,
-        'C:\\path\\to\\your\\directory\\final_image.bmp',
-        'C:\\path\\to\\your\\directory\\metadata.json'
+        BRC_69_MD,
+        'path/to/your/directory/final_image.bmp',
+        'path/to/your/directory/metadata.json'
     )
