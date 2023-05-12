040212ab9708a2060c7b6e118c91068d4d029a63
LawyerCat42069 <133282516+LawyerCat42069@users.noreply.github.com>
2023-05-12 11:19:55 -0700

    Create BRC-69_Omni_Decode_Embed.py

---
+++ OmniDecode+Embed.py
@@ -0,0 +1,63 @@
+import base64
+import json
+import bson
+from PIL import Image
+from io import BytesIO
+from stegano import lsb
+import packet
+
+
+salt = "Salt6969420741!"
+
+# Hash values
+hash_value1 = "paste_hash_value_1_here"
+hash_value2 = "paste_hash_value_2_here"
+
+# Reverse the hashing process to retrieve the base64 strings
+BRC_69 = base64.b64decode(hash_value1[len(salt):]).decode('utf-8')
+BRC_69_MD = base64.b64decode(hash_value2[len(salt):]).decode('utf-8')
+
+
+def decode_base64_to_image(BRC_69, output_path):
+    # Decode base64 string
+    decoded_data = base64.b64decode(BRC_69)
+
+    # Convert decoded data to an image object
+    img_data = BytesIO(decoded_data)
+    image = Image.open(img_data)
+
+    # Save the image as a bitmap
+    image_path = "temp_image.bmp"
+    image.save(image_path, format='BMP')
+
+    return image_path
+
+
+def decode_and_embed_metadata(image_path, BRC_69_MD, output_image_path, output_json_path):
+    # Decode the hidden metadata from base64
+    decoded_metadata = base64.b64decode(BRC_69_MD)
+
+    # Convert the decoded metadata from BSON to JSON
+    try:
+        metadata_dict = bson.loads(decoded_metadata)
+    except bson.InvalidBSON:
+        print(f"Error: The hidden metadata in {image_path} is not a valid BSON string.")
+        return
+
+    # Write the metadata to a JSON file
+    with open(output_json_path, 'w') as json_file:
+        json.dump(metadata_dict, json_file, indent=4)
+
+    # Embed metadata into the image using steganography
+    secret_image = lsb.hide(image_path, str(metadata_dict))
+    secret_image.save(output_image_path)
+
+
+if __name__ == "__main__":
+    image_path = decode_base64_to_image(packet.BRC_69, 'C:\\path\\to\\your\\directory\\decoded_image.bmp')
+    decode_and_embed_metadata(
+        image_path,
+        packet.BRC_69_MD,
+        'C:\\path\\to\\your\\directory\\final_image.bmp',
+        'C:\\path\\to\\your\\directory\\metadata.json'
+    )
