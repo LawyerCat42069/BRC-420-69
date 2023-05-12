6e9d55f0363e51e584c88015888d032c123f9e56
LawyerCat42069 <133282516+LawyerCat42069@users.noreply.github.com>
2023-05-11 22:04:49 -0700

    Update Sample_BRC_69_NoHash.py

--- Sample_BRC_69_NoHash.py
+++ Sample_BRC_69_NoHash.py
@@ -5,9 +5,9 @@ from PIL import Image
 from io import BytesIO
 from stegano import lsb
 
-def decode_base64_to_image(base64_string, output_path, metadata):
+def decode_base64_to_image(base64_string, output_path):
     # Decode base64 string
-    decoded_data = base64.b64decode(base64_imagehash)
+    decoded_data = base64.b64decode(base64_string)
 
     # Convert decoded data to an image object
     img_data = BytesIO(decoded_data)
@@ -15,29 +15,27 @@ def decode_base64_to_image(base64_string, output_path, metadata):
 
     # Save the image as a bitmap
     image_path = "temp_image.bmp"
-    image.save(image_path, format='BMP')
+    image.save(image_path, format='BMP') 
 
-    # Embed metadata into the image using steganography
-    secret_image = lsb.hide(image_path, base64_metadata_hash)
-    secret_image.save(output_path)
+    return image_path
 
-def extract_metadata_to_json(image_path, output_path):
-    # Reveal the hidden metadata from the image
-    hidden_metadata = lsb.reveal(image_path)
-
+def decode_bson(base64_bson_metadata):
     # Decode the hidden metadata from base64
-    decoded_metadata = base64.b64decode(base64_metadata_hash).decode('utf-8')
-  
-    # Convert the decoded metadata from BSON to JSON
+    decoded_metadata = base64.b64decode(base64_bson_metadata)
 
+    # Convert the decoded metadata from BSON to a dictionary
     try:
         metadata_dict = bson.loads(decoded_metadata)
     except bson.InvalidBSON:
-        print(f"Error: The hidden metadata in {image_path} is not a valid BSON string.")
-        return
+        print(f"Error: The hidden metadata is not a valid BSON string.")
+        return None
 
-    # Write the dictionary to a JSON file
-    with open(output_path, 'w') as json_file:
-        json.dump(metadata_dict, json_file, indent=4)
+    return metadata_dict
 
- 
+def embed_metadata(image_path, metadata_dict, output_path):
+    # Convert the metadata dictionary to a JSON string
+    metadata_json = json.dumps(metadata_dict)
+
+    # Embed the metadata into the image using steganography
+    secret_image = lsb.hide(image_path, metadata_json)
+    secret_image.save(output_path)
