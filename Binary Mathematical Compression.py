b51fe0bc6ade0a25c9f612ee2ea72bb861d0f160
LawyerCat42069 <133282516+LawyerCat42069@users.noreply.github.com>
2023-05-13 15:10:38 -0700

    Update Binary Mathematical Compression.py

--- Binary Mathematical Compression.py
+++ Binary Mathematical Compression.py
@@ -1,5 +1,5 @@
+import sys
 import bson
-import math
 
 def create_binary_equation_from_bson(file_path, base_number=2100000000000000):
     # Load the BSON data from file
@@ -9,20 +9,22 @@ def create_binary_equation_from_bson(file_path, base_number=2100000000000000):
     # Extract the binary data from the BSON
     binary_data = bson.loads(bson_data)
 
-    # Convert binary data to an integer
-    integer_value = int.from_bytes(binary_data, byteorder='big')
+    # Convert binary data to a string of ones and zeros
+    binary_string = ''.join(format(byte, '08b') for byte in binary_data)
 
-    # Find the logarithm of the integer value with the given base
-    exponent = math.log(integer_value, base_number)
+    # Convert binary string to a literal number
+    literal_number = int(binary_string, 2)
 
-    # Calculate the nearest whole exponent that does not exceed the integer value
-    exponent = math.floor(exponent)
+    # Calculate the nearest exponent based on the literal number
+    exponent = 0
+    while base_number ** (exponent + 1) <= literal_number:
+        exponent += 1
 
     # Calculate the result using the base number and exponent
     result = base_number ** exponent
 
-    # Calculate the remainder necessary to match the integer value
-    remainder = integer_value - result
+    # Calculate the remainder necessary to match the literal number
+    remainder = literal_number - result
 
     # Create the formula
     equation = f"{base_number}^{exponent}"
@@ -33,45 +35,17 @@ def create_binary_equation_from_bson(file_path, base_number=2100000000000000):
     return equation
 
 
-# Example usage
-bson_file = 'binary_data.bson'
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        bson_file = sys.argv[1]
+    else:
+        bson_file = input("Enter the BSON file path: ")
 
-# Create the binary equation from BSON file (with default base number)
-binary_equation = create_binary_equation_from_bson(bson_file)
+    base_number = 2100000000000000
 
-# Write the equation to a text file
-with open('binary_equation.txt', 'w') as file:
-    file.write(binary_equation)import math
+    # Create the binary equation from BSON file (with specified base number)
+    binary_equation = create_binary_equation_from_bson(bson_file, base_number)
 
-def create_binary_equation(integer_value, base_number):
-    # Find the logarithm of the integer value with the given base
-    exponent = math.log(integer_value, base_number)
-
-    # Calculate the nearest whole exponent that does not exceed the integer value
-    exponent = math.floor(exponent)
-
-    # Calculate the result using the base number and exponent
-    result = base_number ** exponent
-
-    # Calculate the remainder necessary to match the integer value
-    remainder = integer_value - result
-
-    # Create the formula
-    equation = f"{base_number}^{exponent}"
-
-    if remainder > 0:
-        equation += f" + {remainder}"
-
-    return equation
-
-
-# Example usage
-integer_value = 123456789
-base_number = 21000000
-
-# Create the binary equation
-binary_equation = create_binary_equation(integer_value, base_number)
-
-# Write the equation to a text file
-with open('binary_equation.txt', 'w') as file:
-    file.write(binary_equation)
+    # Write the equation to a text file
+    with open('binary_equation.txt', 'w') as file:
+        file.write(binary_equation)
