4e2a70e9868e97fc4ed1f5eb169288db75d062a8
LawyerCat42069 <133282516+LawyerCat42069@users.noreply.github.com>
2023-05-14 12:03:36 -0700

    Create VIEWERSHELL.sh

---
+++ VIEWERSHELL.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+extracted_file_path="/path/to/extracted/file.html"
+
+# Check if the extracted file exists
+if [ ! -f "$extracted_file_path" ]; then
+    echo "File not found: $extracted_file_path"
+    exit 1
+fi
+
+# Open a subshell with restricted privileges
+(
+    # Disable potentially dangerous commands
+    alias rm="echo 'rm command is disabled'"
+    alias mv="echo 'mv command is disabled'"
+    alias cp="echo 'cp command is disabled'"
+    alias mkdir="echo 'mkdir command is disabled'"
+    alias rmdir="echo 'rmdir command is disabled'"
+
+    # Set necessary environment variables for the shell
+    export PAGER="less"
+    export BROWSER="your-preferred-browser"
+    
+    # Change directory to a safe location if needed
+    cd /path/to/safe/location
+    
+    # Run the default shell for viewing the extracted contents
+    $SHELL
+)
